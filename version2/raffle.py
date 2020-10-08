3d245fb0c147b7042d35446eb55be1b5762e9701
Kaytee Cannon <kaytee.cannon@me.com>
2020-10-08 01:01:05 +0000

    Adds raffle functions to version 2

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,29 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+import customers
+import sys
+import random
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        ))
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customer_list = customers.get_customers_from_file("customers.txt")
+    pick_winner(customer_list)
+
+
+run_raffle()
+
+
